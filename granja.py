56c43560a70aff4a7f403ac33334f00a47330cdc
Sergio Gomez <segomezco@unal.edu.co>
2024-11-27 15:39:59 -0500

    Aves

---
+++ granja.py
@@ -0,0 +1,8 @@
+aves=int(input("Ingrese la cantidad de aves del galpón: "))
+def granja(x):
+    gallina=x/3
+    huevo3=(30/3)*(gallina/2)
+    huevo5=(30/5)*(gallina/2)
+    huevomes=huevo5+huevo3
+    return huevomes
+print("Las gallinas del galpón producen", granja(aves), "Huevos al mes.")
